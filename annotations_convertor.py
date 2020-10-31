55e6de9d407640b6d14d681fc7c7c41935a77100
adrian-13 <59730272+adrian-13@users.noreply.github.com>
2020-10-31 19:31:17 +0100

    Update annotations_convertor.py
    - added: ArgumentParser
    - added: print progress every 1000 iterations and total number of images
    - removed: argument number of images in convert function

--- annotations_convertor.py
+++ annotations_convertor.py
@@ -1,11 +1,17 @@
 import json
+import argparse
 
-def convertor(source_path, number_of_images, destination_path):
+def convert(source_path, destination_path):
+  
   with open(source_path) as json_file:
     data = json.load(json_file)
-    
+ 
+    print("Converting...\n")
     counter = 0
+
     for p in data['annotations']:
+      if counter > 0 and counter % 1000 == 0:
+        print("Converted: {0} images".format(counter))  
       for p in data['annotations']:
         if (p['image_id'] == counter):
           print(p['category_id'], end=" ", file=open(destination_path +'/FLIR_0{:04d}.txt'.format(counter + 1), 'a'))
@@ -14,13 +20,23 @@ def convertor(source_path, number_of_images, destination_path):
           print(p['bbox'][2] / 640, end=" ", file=open(destination_path + '/FLIR_0{:04d}.txt'.format(counter + 1), 'a'))
           print(p['bbox'][3] / 512, file=open(destination_path + '/FLIR_0{:04d}.txt'.format(counter + 1), 'a'))
       counter += 1
-      if (counter >= number_of_images):
+      if (counter >= int(p['image_id'])):
+        print("Total: {0} images converted\n".format(counter))
         print("Conversion successfully!" )
         break
 
-        
-source_path = " "
-number_of_images = None
-destination_path = " "
+  return None    
 
-convertor(source_path, number_of_images, destination_path)
+parser = argparse.ArgumentParser()
+parser.add_argument('-src', metavar='path', type=str, required=True, help='path to source folder')
+parser.add_argument('-dst', metavar='path', type=str, required=True, help='path to destination folder')
+args = parser.parse_args()
+
+source_path=args.src
+destination_path=args.dst
+
+def main():
+  convert(source_path, destination_path)
+
+if __name__ == "__main__":
+  main()
